9338e3bdac6f5d677d1f5d390a67df79bb3f6cb9
txm0405 <moritzselter32@gmail.com>
2025-10-16 16:16:27 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { ArrowDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import heroBg from "@/assets/hero-bg.jpg";
+import heroBg from "/hero-bg.webp";
 
 const Hero = () => {
   const scrollToProjects = () => {
